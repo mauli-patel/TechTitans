0679fb543875dab8d64a25d2bc8f83d460affe76
mauli-patel <115323066+mauli-patel@users.noreply.github.com>
2023-07-10 19:20:08 -0400

    Add files via upload

---
+++ src/test/java/org/techtitans/mavenproject/TechTitans/AppTest.java
@@ -0,0 +1,20 @@
+package org.techtitans.mavenproject.TechTitans;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
