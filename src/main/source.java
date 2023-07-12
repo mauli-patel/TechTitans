8ceca324748f9b942ddd7ab2c544648b258a40d9
pranaysood <50906826+pranaysood@users.noreply.github.com>
2023-07-12 13:32:25 -0400

    Update source.java

---
+++ src/main/source.java
@@ -0,0 +1,10 @@
+private static void initializeSystem(int n) {
+        size = n;
+        floor = new int[size][size];
+        posX = 0;
+        posY = 0;
+        isPenDown = false;
+        facingDirection = "north";
+    }
+
+  
