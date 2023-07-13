59f0f41757c74ec89c7da89a1a8b86122c6cb5cd
mauli-patel <115323066+mauli-patel@users.noreply.github.com>
2023-07-13 18:44:36 -0400

    Update source.java

--- src/main/source.java
+++ src/main/source.java
@@ -1,149 +1,112 @@
 import java.util.Scanner;
 
 public class Robot {
-	private static int[][] board;
-    private static int dimension;
-    private static int Xaxis;
-    private static int Yaxis;
+    private static int[][] board;
+    private static int size;
+    private static int posX;
+    private static int posY;
     private static boolean isPenDown;
     private static String Direction;
-    
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String command;
 
-        System.out.print("Enter your command: ");
+        System.out.print("Enter the command: ");
         while (!(command = scanner.nextLine()).equalsIgnoreCase("Q")) {
             executeCommand(command);
-            System.out.print("Enter your command: ");
+            System.out.print("Enter the command: ");
+        }
+    }
 
-	}
-
-}
-	private static void executeCommand(String command) {
+    private static void executeCommand(String command) {
         char commandType = command.charAt(0);
         String arguments = command.substring(1).trim();
 
-        switch (commandType) {
-            case 'I':
-                initializeSystem(Integer.parseInt(arguments));
-                break;
-            case 'C':
-                printCurrentPosition();
-                break;
-            case 'D':
-                setPenDown(true);
-                break;
-            case 'U':
-                setPenDown(false);
-                break;
-            case 'R':
-                turnRight();
-                break;
-            case 'L':
-                turnLeft();
-                break;
-            case 'M':
-                move(Integer.parseInt(arguments));
-                break;
-            case 'P':
-                printFloor();
-                break;
-            default:
-                System.out.println("Invalid command.");
-        }
+        if (commandType == 'I'){initialize_System(Integer.parseInt(arguments));}
+        else if(commandType == 'C'){print_CurrentPosition();}
+        else if(commandType == 'D'){setPenDown(true);}
+        else if(commandType == 'U'){setPenDown(false);}
+        else if(commandType == 'R'){turn_Right();}
+        else if(commandType == 'L'){turn_Left();}
+        else if(commandType == 'M'){move(Integer.parseInt(arguments));}
+        else if(commandType == 'P'){print_board();}
+        else {System.out.println("Invalid command.");}
+ 
     }
-	private static void initializeSystem(int n) {
-        dimension = n;
-        board = new int[dimension][dimension];
-        Xaxis = 0;
-        Yaxis = 0;
+
+    private static void initialize_System(int n) {
+        size = n;
+        board = new int[size][size];
+        posX = 0;
+        posY = 0;
         isPenDown = false;
         Direction = "north";
     }
-  private static void printCurrentPosition() {
+
+    private static void print_CurrentPosition() {
         System.out.printf("Position: %d, %d - Pen: %s - Facing: %s%n",
-                Xaxis, Yaxis, (isPenDown ? "down" : "up"), Direction);
+                posX, posY, (isPenDown ? "down" : "up"), Direction);
     }
-  
-  private static void setPenDown(boolean down) {
-      isPenDown = down;
-  }
-  
-  private static void turnRight() {
-      switch (Direction) {
-          case "north":
-              Direction = "east";
-              break;
-          case "east":
-              Direction = "south";
-              break;
-          case "south":
-              Direction = "west";
-              break;
-          case "west":
-              Direction = "north";
-              break;
-      }
-  }
 
-  private static void turnLeft() {
-      switch (Direction) {
-          case "north":
-              Direction = "west";
-              break;
-          case "east":
-              Direction = "north";
-              break;
-          case "south":
-              Direction = "east";
-              break;
-          case "west":
-              Direction = "south";
-              break;
-      }
-  }
+    private static void setPenDown(boolean down) {
+        isPenDown = down;
+    }
 
-  private static void move(int steps) {
-      switch (Direction) {
-          case "north":
-              for (int i = 0; i < steps; i++) {
-                  Yaxis = Math.min(Yaxis + 1, dimension - 1);
-                  if (isPenDown) {
-                      board[Yaxis][Xaxis] = 1;
-                  }
-              }
-              break;
-          case "east":
-              for (int i = 0; i < steps; i++) {
-                  Xaxis = Math.min(Xaxis + 1, dimension - 1);
-                  if (isPenDown) {
-                      board[Yaxis][Xaxis] = 1;
-                  }
-              }
-              break;
-          case "south":
-              for (int i = 0; i < steps; i++) {
-                  Yaxis = Math.max(Yaxis - 1, 0);
-                  if (isPenDown) {
-                      board[Yaxis][Xaxis] = 1;
-                  }
-              }
-              break;
-          case "west":
-              for (int i = 0; i < steps; i++) {
-                  Xaxis = Math.max(Xaxis - 1, 0);
-                  if (isPenDown) {
-                      board[Yaxis][Xaxis] = 1;
-                  }
-              }
-              break;
-      }
-  }
- private static void printFloor() {
-        for (int i = dimension - 1; i >= 0; i--) {
+    private static void turn_Right() {
+       if (Direction == "north"){ Direction = "east"; }
+       else if (Direction == "east") { Direction = "south"; }
+       else if (Direction == "south") { Direction = "west"; }
+       else  { Direction = "north"; }
+    }
+    
+    private static void turn_Left() {
+    	 if (Direction == "north"){ Direction = "west"; }
+         else if (Direction == "east") { Direction = "north"; }
+         else if (Direction == "south") { Direction = "east"; }
+         else  { Direction = "south"; }
+    }
+
+    private static void move(int steps) {
+    	 if (Direction == "north"){
+                for (int i = 0; i < steps; i++) {
+                    posY = Math.min(posY + 1, size - 1);
+                    if (isPenDown) {
+                        board[posY][posX] = 1;
+                    }
+                }
+    	 }
+    	 else if (Direction == "east"){
+                for (int i = 0; i < steps; i++) {
+                    posX = Math.min(posX + 1, size - 1);
+                    if (isPenDown) {
+                        board[posY][posX] = 1;
+                    }
+                }
+    	 }
+    	 else if (Direction == "south") {
+                for (int i = 0; i < steps; i++) {
+                    posY = Math.max(posY - 1, 0);
+                    if (isPenDown) {
+                        board[posY][posX] = 1;
+                    }
+                }
+    	 }
+    	 else {
+                for (int i = 0; i < steps; i++) {
+                    posX = Math.max(posX - 1, 0);
+                    if (isPenDown) {
+                        board[posY][posX] = 1;
+                    }
+                }
+    	 }
+     }
+    
+
+    private static void print_board() {
+        for (int i = size - 1; i >= 0; i--) {
             System.out.printf("%2d ", i);
-            for (int j = 0; j < dimension; j++) {
+            for (int j = 0; j < size; j++) {
                 if (board[i][j] == 1) {
                     System.out.print("* ");
                 } else {
@@ -153,7 +116,7 @@ public class Robot {
             System.out.println();
         }
         System.out.print("   ");
-        for (int i = 0; i < dimension; i++) {
+        for (int i = 0; i < size; i++) {
             System.out.printf("%2d", i);
         }
         System.out.println();
