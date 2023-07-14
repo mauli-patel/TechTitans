7e0e8d0722f4d67a0b5314d86b21a2d514be5f47
pranaysood <50906826+pranaysood@users.noreply.github.com>
2023-07-14 00:32:12 -0400

    Update unittest.java

--- src/test/unittest.java
+++ src/test/unittest.java
@@ -15,3 +15,30 @@ public class UnitTest {
         robot = new Robot();
     }
 
+	@Test
+    public void testInitializeSystem() {
+        
+        robot.initialize_System(5);
+
+        assertEquals(5, robot.dimension);
+        assertEquals(0, robot.Xaxis);
+        assertEquals(0, robot.Yaxis);
+        assertEquals(false, robot.PenDown);
+        assertEquals("north", robot.Direction);
+        assertEquals(5, robot.board.length);
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                assertEquals(0, robot.board[i][j]);
+            }
+        }
+    }
+
+    @Test
+    public void testSetPenDown() {
+        robot.setPenDown(true);
+        assertEquals(true, robot.PenDown);
+
+        robot.setPenDown(false);
+        assertEquals(false, robot.PenDown);
+    }
+
